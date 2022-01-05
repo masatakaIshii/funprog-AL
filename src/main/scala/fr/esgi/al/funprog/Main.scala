5e15202ad898b0df5cb24533421790a0d47f1e0e
MasatakaIshii <masaradio78@gmail.com>
2022-01-05 02:40:34 +0100

    Delete unused usecase get points of others to manage collisions

--- src/main/scala/fr/esgi/al/funprog/Main.scala
+++ src/main/scala/fr/esgi/al/funprog/Main.scala
@@ -1,21 +1,21 @@
 package fr.esgi.al.funprog
 
-import fr.esgi.al.funprog.usecase.{CreateJsonFromResults, GetPointsOfOthers, ReadFileInstructions, SaveToJsonFile, StartGivenLawnMower}
+import fr.esgi.al.funprog.usecase.{CreateJsonFromResults, ReadFileInstructions, SaveToJsonFile, StartGivenLawnMower}
 
 
 object Main extends App {
   println("Ici le programme principal")
   val test = ReadFileInstructions().execute("tondeuse.txt")
-//  val funProg =FunProg.apply(getPointsOfOthers = GetPointsOfOthers(), startGivenLawnMower = StartGivenLawnMower())
-//  val uesh = funProg.start(test._1, test._2)
-//
-//  println(uesh)
-//  val json = CreateJsonFromResults().execute(uesh, test._1)
-//  println(json)
-//
-//  val filePath = SaveToJsonFile.apply().execute(json,"result.json")
-//
-//  println("Result is save at the path : " + filePath)
+  val funProg =FunProg.apply(startGivenLawnMower = StartGivenLawnMower())
+  val uesh = funProg.start(test._1, test._2)
+
+  println(uesh)
+  val json = CreateJsonFromResults().execute(uesh, test._1)
+  println(json)
+
+  val filePath = SaveToJsonFile.apply().execute(json,"result.json")
+
+  println("Result is save at the path : " + filePath)
 
 
 
